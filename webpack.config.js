a21f1e2b60dfbd757c35f72dce69b94f92cbc9fa
ak2403 <arunpasupathi24@gmail.com>
2022-05-20 18:08:28 +1000

    setup: added basic html template for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
   mode: 'development',
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template/index.html',
+    }),
+  ],
 };
