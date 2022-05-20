f95c0ccf28e755dcb94742f42b938730078512e1
ak2403 <arunpasupathi24@gmail.com>
2022-05-20 17:13:17 +1000

    setup: added eslint and prettier for code standard

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
-const path = require('path')
-const htmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   mode: 'development',
-  plugins: [
-new htmlWebpackPlugin()
-  ]
-}
+  plugins: [new HtmlWebpackPlugin()],
+};
